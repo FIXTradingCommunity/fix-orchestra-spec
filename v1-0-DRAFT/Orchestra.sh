4aa9e6f500eef733725eebef9ee5b0b44ba4cada
Hanno Klein <hanno@fixdom.de>
2020-02-11 13:17:36 +0100

    New title page generation
    Used new master style docx for Technical Standards. Disclaimer specific text for RC, DS, FINAL is now part of YAML file. Disclaimer now a central file stored with FIX Session Layer only.

--- v1-0-DRAFT/Orchestra.sh
+++ v1-0-DRAFT/Orchestra.sh
@@ -1,5 +1,13 @@
 echo Compilation started...
 
-pandoc FIXDisclaimerTechStdDS.md orchestra_spec.md -o "FIX Orchestra V1.0 Draft Standard.docx" --reference-doc=../FIX_TechStd_Style.docx --metadata-file=Orchestra.yaml --toc --toc-depth=4
+pandoc FIXDisclaimerTechStd.md orchestra_spec.md -o "FIX Orchestra V1.0 Draft Standard.docx" --reference-doc=../FIX_TechStd_Style_MASTER.docx --metadata-file=Orchestra.yaml --toc --toc-depth=4
+pandoc orchestra_spec.md -o OrchestraONLINE.html
+
+# Create separate online versions for production and test website by including appropriate link prefixes
+sed s/'img src="media\/'/'img src="https:\/\/www.fixtrading.org\/wp-content\/uploads\/2020\/02\/'/ OrchestraONLINE.html > OrchestraONLINE_PROD.html
+sed s/'img src="media\/'/'img src="https:\/\/www.technical-fixprotocol.org\/wp-content\/uploads\/2020\/02\/'/ OrchestraONLINE.html > OrchestraONLINE_TEST.html
+# Change remaining links to production website in test version to test website
+sed -i '.bak' s/www.fixtrading.org/www.technical-fixprotocol.org/ OrchestraONLINE_TEST.html
+echo Orchestra ONLINE version created for PROD and TEST
 
 echo Compilation ended!
