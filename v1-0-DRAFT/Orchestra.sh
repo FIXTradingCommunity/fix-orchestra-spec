08b082b32614eeef156bd09af8ee69dd8930e038
Hanno Klein <hanno@fixdom.de>
2020-04-11 19:10:45 +0200

    Script simplification

--- v1-0-DRAFT/Orchestra.sh
+++ v1-0-DRAFT/Orchestra.sh
@@ -7,13 +7,13 @@ TARGET="$PWD"
 echo Compilation started...
 LOCAL="<INSERT YOUR LOCAL PATH TO THE GITHUB CLONE DIRECTORY HERE>"
 ROOT="$LOCAL/GitHub/fix-orchestra-spec/v1-0-DRAFT"
-# There is only one disclaimer and style docx file for all FIX Technical Standards and it is stored with teh FIX Session Layer
+# There is only one disclaimer and style docx file for all FIX Technical Standards and it is stored with the FIX Session Layer
 DISCLAIMER="$LOCAL/GitHub/fix-session-layer-standards/FIXDisclaimerTechStd.md"
 STYLE="$LOCAL/GitHub/fix-session-layer-standards/FIX_TechStd_Style_MASTER.docx"
 SOURCE="$ROOT"
 YAML="$SOURCE/Orchestra.yaml"
 FILES="orchestra_spec.md"
-WPFOLDER="wp-content\/uploads\/2020\/02"
+WPFOLDER="/wp-content/uploads/2020/02/"
 
 cd "$SOURCE"
 
@@ -28,8 +28,8 @@ pandoc $FILES -o "$TARGET/OrchestraONLINE.html"
 cd "$TARGET"
 
 # Create separate online versions for production and test website by including appropriate link prefixes
-sed s/"img src=\"media\/"/"img src=\"https:\/\/www.fixtrading.org\/$WPFOLDER\/"/g OrchestraONLINE.html > OrchestraONLINE_PROD.html
-sed s/"img src=\"media\/"/"img src=\"https:\/\/www.technical-fixprotocol.org\/$WPFOLDER\/"/g OrchestraONLINE.html > OrchestraONLINE_TEST.html
+sed 's,img src="media/,img src="https://www.fixtrading.org'$WPFOLDER',g' OrchestraONLINE.html > OrchestraONLINE_PROD.html
+sed 's,img src="media/,img src="https://www.technical-fixprotocol.org'$WPFOLDER',g' OrchestraONLINE.html > OrchestraONLINE_TEST.html
 
 # Change remaining links to production website in test version to test website
 sed -i '.bak' s/www.fixtrading.org/www.technical-fixprotocol.org/ OrchestraONLINE_TEST.html
